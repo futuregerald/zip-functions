b754608b2276bfaa09d3722b93ac30974a30092b
Gerald O <gerald.onyango@gmail.com>
2018-09-05 22:26:20 -0400

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+## zip functions
+
+This is just a small library for zipping javascript functions with node_modules for deploying on Netlify, though I see no reason why it won't work just fine deploying to lambda directly.
---
+++ zipfunctions.js
@@ -0,0 +1,25 @@
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+
+console.log(__dirname);
+fs.readdir(__dirname, (err, files) => {
+  if (err) {
+    return console.log(`couldn't scan current directory: ${err}`);
+  }
+  // console.log(files);
+  const funcFiles = files.filter(f => {
+    const regex = /\.js$/g;
+    return regex.test(f);
+  });
+  funcFiles.forEach(script => {
+    if (script !== 'zip_functions.js') {
+      const name = script.substring(0, script.length - 3);
+      const zip = new AdmZip();
+      zip.addLocalFile(script, `/${name}/`);
+      zip.addLocalFolder('./node_modules/', `/${name}/node_modules`);
+      zip.addLocalFolder('./apis/', `/${name}/apis`);
+      zip.addLocalFolder('./helpers/', `/${name}/helpers`);
+      zip.writeZip(`./zipped/${name}.zip`);
+    }
+  });
+});
